9114efbf0d3b20f2db54b6169b95c7c06b184f37
Ryan Connelly <ryan@outs.in>
2012-09-29 12:43:37 +1000

    teamchat

---
+++ teamchat.py
@@ -0,0 +1,19 @@
+# teamchat.py
+# Shows team chat in IRC, colored appropriately.
+# by topologist
+
+def apply_script(protocol, connection, config):
+    class TeamChatConnection(connection):
+        def on_chat(self, value, is_global):
+            if is_global or not self.protocol.irc_relay:
+                return connection.on_chat(self, value, is_global)
+            if self.team == self.protocol.blue_team:
+                message = '\x0302'
+            elif self.team == self.protocol.green_team:
+                message = '\x0303'
+            else:
+                message = '\x02'
+            message += '%s: %s' % (self.name, value)
+            self.protocol.irc_relay.send(message, filter = False)
+            return connection.on_chat(self, value, is_global)
+    return protocol, TeamChatConnection
