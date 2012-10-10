93b48ba3e0b59badbe98346df840c199f6735bf4
Ryan Connelly <ryan@outs.in>
2012-10-11 00:09:14 +1100

    spec teamchat is now yellow

--- teamchat.py
+++ teamchat.py
@@ -7,15 +7,13 @@ def apply_script(protocol, connection, config):
         def on_chat(self, value, is_global):
             if is_global or not self.protocol.irc_relay:
                 return connection.on_chat(self, value, is_global)
-            bold = False
             if self.team == self.protocol.blue_team:
                 message = '<\x0302'
             elif self.team == self.protocol.green_team:
                 message = '<\x0303'
             else:
-                message = '<\x02'
-                bold = True
-            message += '%s%s> %s' % (self.name, '\x02' if bold else '\x03', value)
+                message = '<\x0307'
+            message += '%s%s> %s' % (self.name, '\x03', value)
             self.protocol.irc_relay.send(message, filter = False)
             return connection.on_chat(self, value, is_global)
     return protocol, TeamChatConnection
