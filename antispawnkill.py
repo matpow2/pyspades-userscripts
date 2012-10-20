3bc4eae9b252bb393951f280957fea7b2a0f4609
Ryan Connelly <ryan@outs.in>
2012-10-20 19:01:37 +1100

    modified antispawnkill.py to make players invisible during antikill phase, fixed setting of ask time

--- antispawnkill.py
+++ antispawnkill.py
@@ -5,7 +5,10 @@ Usage: /ask <seconds for a player to be invulnerable>
 I'm capping it at 5 by default so people don't accidentally make everyone invincible.
 """
 from time import time
+from scheduler import Scheduler
 from commands import add, admin, name
+from pyspades.server import create_player, set_tool, set_color, input_data, weapon_input
+from pyspades.common import make_color
 
 MAX_SECS_NODAMAGE = 5
 
@@ -13,10 +16,59 @@ MAX_SECS_NODAMAGE = 5
 @name('ask')
 def antispawnkill(connection, seconds):
     protocol = connection.protocol
+    seconds = int(seconds)
     protocol.ask_time = seconds if seconds <= MAX_SECS_NODAMAGE else MAX_SECS_NODAMAGE
-    connection.send_chat("Anti-spawnkill time set to %s seconds" % protocol.ask_time)
+    connection.send_chat("Anti-spawnkill time set to %s seconds by %s" % ( protocol.ask_time, connection.name ), irc = True)
 
 add(antispawnkill)
+
+# Not the best solution, but it works. Just copied the normal inv function
+# and removed anti-kill stuff.
+def my_invisible(connection):
+    protocol = connection.protocol
+
+    player = connection
+
+    player.invisible = not player.invisible
+    player.filter_visibility_data = player.invisible
+
+    player.send_chat("now visible") if not player.invisible
+
+    if not player.invisible:
+        x, y, z = player.world_object.position.get()
+        create_player.player_id = player.player_id
+        create_player.name = player.name
+        create_player.x = x
+        create_player.y = y
+        create_player.z = z
+        create_player.weapon = player.weapon
+        create_player.team = player.team.id
+
+        world_object = player.world_object
+
+        input_data.player_id = player.player_id
+        input_data.up = world_object.up
+        input_data.down = world_object.down
+        input_data.left = world_object.left
+        input_data.right = world_object.right
+        input_data.jump = world_object.jump
+        input_data.crouch = world_object.crouch
+        input_data.sneak = world_object.sneak
+        input_data.sprint = world_object.sprint
+
+        set_tool.player_id = player.player_id
+        set_tool.value = player.tool
+        set_color.player_id = player.player_id
+        set_color.value = make_color(*player.color)
+
+        weapon_input.primary = world_object.primary_fire
+        weapon_input.secondary = world_object.secondary_fire
+
+        protocol.send_contained(create_player, sender = player, save = True)
+        protocol.send_contained(set_tool, sender = player)
+        protocol.send_contained(set_color, sender = player, save = True)
+        protocol.send_contained(input_data, sender = player)
+        protocol.send_contained(weapon_input, sender = player)
 
 def apply_script(protocol, connection, config):
     class ASKProtocol(protocol):
@@ -33,6 +85,12 @@ def apply_script(protocol, connection, config):
         
         def on_spawn(self, pos):
             self.spawn_time = int( time() )
+            my_invisible(self)
+            schedule = Scheduler(self.protocol)
+            schedule.call_later(self.protocol.ask_time, self.uninvis)
             return connection.on_spawn(self, pos)
+
+        def uninvis(self):
+            my_invisible(self)
 
     return ASKProtocol, ASKConnection
