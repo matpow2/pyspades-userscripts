0c5ede511408e817ff2e1bc32639c20a116b974d
Ryan Connelly <ryan@outs.in>
2012-10-24 22:31:06 +1100

    removesquad.py

---
+++ removesquad.py
@@ -0,0 +1,28 @@
+"""
+removesquad.py
+
+Removes a player from their current squad, if any.
+Useful in conjunction with the jail script.
+
+REQUIRES SQUAD.PY
+"""
+
+from commands import admin, name, get_player, add
+
+@admin
+@name('rs')
+def remove_squad(self, player_name):
+    player = get_player(self.protocol, player_name)
+    
+    if player.squad is not None:
+        player.squad = None
+        player.squad_pref = None
+        self.join_squad(squad, squad_pref)
+        return 'Removed player %s from their squad.' % player.name
+    else:
+        return 'Player %s is not in a squad!' % player.name
+
+add(remove_squad)
+
+def apply_script(protocol, connection, config):
+    return protocol, connection
